0bc627695ceab9910292fef808186ddba75fd838
m1m23a34 <m1m23a34@oit.ac.jp>
2023-06-12 11:03:46 +0900

    add semonster player class

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("SEMonster!");
-    Monster monster = new Monster();
+    //Monster monster = new Monster();
+    Player monster = new Player();
     System.out.println(monster.toString());
+    monster.drawMonsters();
+    monster.showDeck();
   }
 }
